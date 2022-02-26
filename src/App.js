1389dd9e923243ab6a37c751967147686c5e4e0b
SATISH SONKAR <satish.sonkar.ext@siemens-healthineers.com>
2022-02-26 15:02:40 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './css/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='bjk-nav'>
+        <div class="d-flex justify-content-between">
+          <div className="p-2 bd-highlight"><i className="humberger-bar fa-solid fa-bars"></i></div>
+          <div className="p-2 bd-highlight"><img className='logo-name' src='../Images/logo/bjkLogoName.png' /></div>
+          <div className="p-2 bd-highlight"><img className='logo' src='../Images/logo/bjkLogo64.png' /></div>
+        </div>
+      </div>
+      <div className='left-menu'></div>
+      <div className='bjk-footer'>
+      <div className="d-flex bd-highlight">
+      <div className="p-2 flex-fill bd-highlight"><i className="control-btn fa-solid fa-angle-up"></i></div>
+      <div className="p-2 flex-fill bd-highlight"><i className="control-btn fa-solid fa-angle-up"></i></div>
+      <div className="p-2 flex-fill bd-highlight"><i className="control-btn fa-solid fa-angle-up"></i></div>
+      <div className="p-2 flex-fill bd-highlight"><i className="control-btn fa-solid fa-angle-up"></i></div>
+        </div>
+      </div>
+    </>
   );
 }
 
