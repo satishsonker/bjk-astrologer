7c292574eccecc91854b4b8031b9bf8adbd165b7
Satish Sonkar <satish.sonkar@nagarro.com>
2022-04-16 17:12:19 +0530

    update

--- src/components/Banner/PaymentBanner.js
+++ src/components/Banner/PaymentBanner.js
@@ -8,11 +8,11 @@ export default function PaymentBanner() {
         <div className='pay-banner-container'>
             <h3 className='banner-text'>{t("secure")} {t("payment")} {t("options")}</h3>
             <div className='row row-cols-3 row-cols-md-6 pay-banner-list'>
-            <div className="col"><img alt="Paytm Logo" src='../images/paytm.png'/></div>
-                <div className="col"><img alt="Visa Logo" src='../images/visa.png'/></div>
-                <div className="col"><img alt="Master Card Logo" src='../images/mastercard.png'/></div>
-                <div className="col"><img alt="Google Pay Logo" src='../images/gpay.png'/></div>
-                <div className="col"><img alt="SSL Logo" src='../images/ssl.png'/></div>
+            <div className="col"><img alt="Paytm Logo" src='/images/paytm.png'/></div>
+                <div className="col"><img alt="Visa Logo" src='/images/visa.png'/></div>
+                <div className="col"><img alt="Master Card Logo" src='/images/mastercard.png'/></div>
+                <div className="col"><img alt="Google Pay Logo" src='/images/gpay.png'/></div>
+                <div className="col"><img alt="SSL Logo" src='/images/ssl.png'/></div>
             </div>
         </div>
     )
